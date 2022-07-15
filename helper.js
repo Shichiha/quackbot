caece3031da3d0aa6e591579cf93735bf244f7f7
Nanako <00sh.chiha+github@gmail.com>
2022-07-15 17:47:22 +0900

    safe_eval and stuff
    - move command class to helper.js

--- helper.js
+++ helper.js
@@ -1,38 +1,36 @@
-const os = require('os');
-function msToRelativeTime(ms) {
-    var seconds = Math.floor((ms / 1000) % 60);
-    var decimal = Math.floor((ms / 1000) % 1 * 10);
-    var minutes = Math.floor((ms / (1000 * 60)) % 60);
-    var hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(ms / (1000 * 60 * 60 * 24));
-    var result = "";
-    if (days > 0) {
-        result += days + "d ";
-    }
-    if (hours > 0) {
-        result += hours + "h ";
-    }
-    if (minutes > 0) {
-        result += minutes + "m ";
-    }
-    if (seconds > 0) {
-        result += seconds + "s ";
-    }
-    if (decimal > 0) {
-        result += decimal + "ms";
-    }
-    return result;
+function msToRelativeTime (ms) {
+  var seconds = Math.floor((ms / 1000) % 60)
+  var decimal = Math.floor(((ms / 1000) % 1) * 10)
+  var minutes = Math.floor((ms / (1000 * 60)) % 60)
+  var hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+  var days = Math.floor(ms / (1000 * 60 * 60 * 24))
+  var result = ''
+  if (days > 0) {
+    result += days + 'd '
+  }
+  if (hours > 0) {
+    result += hours + 'h '
+  }
+  if (minutes > 0) {
+    result += minutes + 'm '
+  }
+  if (seconds > 0) {
+    result += seconds + 's '
+  }
+  if (decimal > 0) {
+    result += decimal + 'ms'
+  }
+  return result
 }
-
-function getPCSpecs() {
-    var specs = {};
-    specs = {
-        platform: os.platform(),
-        release: os.release(),
-        uptime: os.uptime(),
-        memory: os.freemem()
-    };
-    return specs
+class Command {
+  constructor (name, description, usage, cmd_function) {
+    this.name = name
+    this.description = description
+    this.usage = usage
+    this.cmd_function = cmd_function
+  }
 }
-console.log(getPCSpecs());
-module.exports = { msToRelativeTime };
+module.exports = {
+    msToRelativeTime,
+    Command
+}
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js-selfbot-v13')
 const { token } = require('./config.json')
-const helper = require('./helper.js')
 const logger = require('node-color-log')
 const activities = require('./activities.json')
+const safeEval = require('safe-eval')
+const {msToRelativeTime, Command} = require('./helper.js')
 logger.setDate(() => new Date().toLocaleTimeString())
 
 const client = new Discord.Client({
@@ -20,14 +21,6 @@ client.on('ready', () => {
   }, 5000)
 })
 
-class Command {
-  constructor (name, description, usage, cmd_function) {
-    this.name = name
-    this.description = description
-    this.usage = usage
-    this.cmd_function = cmd_function
-  }
-}
 const messageCreateCommands = []
 messageCreateCommands.push(
   new Command('ping', '?', 'ping', message => {
@@ -35,7 +28,7 @@ messageCreateCommands.push(
   }),
   new Command('uptime', 'How long the bot has been on', 'uptime', message => {
     let uptime = client.uptime
-    let uptime_r = helper.msToRelativeTime(uptime) // Relative Time
+    let uptime_r = `${msToRelativeTime(uptime)} ago`
     message.channel.send(`Uptime: ${uptime_r}`)
   }),
   new Command('help', 'All the functions and their usage', 'help', message => {
@@ -44,6 +37,21 @@ messageCreateCommands.push(
       help_message += `${messageCreateCommands[i].name}: ${messageCreateCommands[i].description}\nUsage: ${messageCreateCommands[i].usage}\n\n`
     }
     message.channel.send(help_message)
+  }),
+  new Command('eval', 'Evaluates a code snippet', 'eval', message => {
+    code = ''
+    try {
+      let code = message.content.split('```')[1].split('```')[0]
+      logger.info(`${message.author.username} evaled: ${code}`)
+      try {
+        let result = safeEval(code)
+        message.channel.send(`Result: ${result}`)
+      } catch (err) {
+        message.channel.send(`${err}`)
+      }
+    } catch (error) {
+      message.channel.send(`Error: ${error}`)
+    }
   })
 )
 
@@ -63,9 +71,7 @@ client.on('messageCreate', async message => {
         },
         `Command Activated: ${messageCreateCommands[i].name}`
       )
-
     }
   }
 })
-
 client.login(token)
