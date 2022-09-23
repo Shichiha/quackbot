e623c941f13b96a238bfd477d4f4380f09479808
Nanako <00sh.chiha@gmail.com>
2022-09-23 14:44:02 +0800

    Update example

--- src/config.example.ts
+++ src/config.example.ts
@@ -3,3 +3,4 @@ export const prefix = "uwu";
 export const activities = ["with a fork"];
 export const locations = ["outside"];
 export const blacklist = ["926104534723111906", "959805847268494401"];
+export const admin = ["703461363573456928"];
